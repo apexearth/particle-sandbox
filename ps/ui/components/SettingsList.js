c369eeafc9d48b5267a9faf45ecd647cc01ee45d
Christopher Jacobs <apexearth@gmail.com>
2017-05-15 09:25:28 -0700

    Title, settings list...

--- ps/ui/components/SettingsList.js
+++ ps/ui/components/SettingsList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SettingInput from './SettingInput'
+import Title from './Title'
 
-const SettingsList = ({object}) => {
+const SettingsList = ({title, object}) => {
     const keys  = Object.keys(object)
     const array = keys.map(key => <SettingInput object={object} key={key}/>)
     return (
         <div className="settings-list">
+            <Title>{title}</Title>
             {array}
         </div>
     )
---
+++ ps/ui/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = ({children}) => (
+    <div>
+        <h3>{children}</h3>
+    </div>
+)
+
+export default Title
