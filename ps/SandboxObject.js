dca37914ef4c944c5a17593938da95c215b453f7
Christopher Jacobs <cjacobs@drobo.com>
2017-05-31 19:11:10 -0700

    Start work on a generator object.

---
+++ ps/SandboxObject.js
@@ -0,0 +1,31 @@
+const PIXI = typeof window !== 'undefined' ? require('pixi.js') : null
+
+class SandboxObject {
+    constructor({parent, position}) {
+        if (!parent) throw new Error('No parent recieved.')
+        this.parent = parent
+        position    = position || {x: 0, y: 0}
+
+        if (typeof window !== 'undefined') {
+            this.container = new PIXI.Container()
+            this.graphics  = new PIXI.Graphics()
+            this.container.addChild(this.graphics)
+            this.parent.container.addChild(this.container)
+        } else {
+            this.container = {position: {x: 0, y: 0}, scale: {x: 1, y: 1}}
+        }
+
+        this.position.x = position.x
+        this.position.y = position.y
+    }
+
+    get position() {
+        return this.container.position
+    }
+
+    get scale() {
+        return this.container.scale
+    }
+}
+
+module.exports = SandboxObject
