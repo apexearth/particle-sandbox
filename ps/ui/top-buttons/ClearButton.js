65496b646e666f53c8bb22ff6ecb6e70b1e99406
cjacobs <apexearth@gmail.com>
2017-07-08 12:47:44 -0700

    Change clear button so it requires a double-tap to clear. So some hard working individual is less likely to lose their work.

--- ps/ui/top-buttons/ClearButton.js
+++ ps/ui/top-buttons/ClearButton.js
@@ -2,12 +2,28 @@ import React from 'react'
 import state from '../state'
 
 class ClearButton extends React.Component {
+    componentWillMount() {
+        this.setState({count: 0})
+    }
+
     render() {
         let {ps}      = state
-        let className = "square-button"
+        let className = this.state.count === 1 ? "square-button square-button-warning" : "square-button"
         return (
             <div className={className}
-                 onClick={() => ps.removeAll()}>
+                 onClick={() => {
+                     if (this.state.count === 1) {
+                         ps.removeAll()
+                         clearTimeout(this.state.timeoutId)
+                         this.setState({count: 0, timeout: null})
+                     } else {
+                         this.setState({
+                             count    : 1,
+                             timeoutId: setTimeout(() => this.setState({count: 0}), 1000)
+                         })
+
+                     }
+                 }}>
                 <span className="glyphicon glyphicon-remove"/>
             </div>
         )
