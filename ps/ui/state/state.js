04ea538a7b430c1180eb427368037e7316d7db0f
cjacobs <apexearth@gmail.com>
2017-07-16 11:07:54 -0700

    Fix a bug in the selection info.

--- ps/ui/state/state.js
+++ ps/ui/state/state.js
@@ -6,13 +6,12 @@ const state = module.exports = new EventEmitter()
 if (typeof window !== 'undefined') {
     window.state = state
 }
-state.actions = require('./actions')
-
-state.screen = 'TitleScreen'
 
 state.deploymentType = "web"
 
-let ps = state.ps = null
+state.actions = require('./actions')
+
+state.screen = 'TitleScreen'
 
 state.subscribe = fn => state.on('state', fn)
 state.notify    = () => state.emit('state', state)
@@ -24,10 +23,6 @@ state.toggleMenu = val => {
     else state.menu = val
     state.menus.forEach(menu => menu.notify())
     analytics.event('state', 'toggleMenu', val)
-}
-
-state.initialize = instance => {
-    ps = state.ps = instance
 }
 
 const createMenu = (name) => {
@@ -61,10 +56,10 @@ const selectionInfo = state.selectionInfo = {
     subscribe: fn => state.on('selectionInfo', fn),
     notify   : () => state.emit('selectionInfo', selectionInfo),
     toggleFollowSelection() {
-        ps.modes.followSelection = !ps.modes.followSelection
+        state.ps.modes.followSelection = !state.ps.modes.followSelection
         state.selectionInfo.notify()
     },
     get followSelection() {
-        return ps.modes.followSelection
+        return state.ps.modes.followSelection
     }
 }
